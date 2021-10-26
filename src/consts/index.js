0991fc1a09a3c86a215c47dba59f5a22372203c5
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-10-26 20:04:26 +0200

    Fix spelling

--- src/consts/index.js
+++ src/consts/index.js
@@ -65,7 +65,7 @@ export const firstPageContent = [
     ],
   },
   {
-    key: "Dodatna objašnjena...",
+    key: "Dodatna objašnjenja...",
     info: "U prethodnom dijelu je objašnjena uloga svake boje. Nakon što odaberete jedan od ponuđenih termina označen zelenom bojom potrebno je upisati vaše ime i prezime. Nakon što klikom na tipku Dodaj dodate vlastito ime i prezime vaš se termin smatra zauzetim i obojat će se crvenom bojom. Kako bi naknadno mogli promijeti ime i prezime ili ukloniti odabrani termin, kraj vašeg termina će se pojaviti simbol olovčice koji Vam omogućuje da napravite navedene promjene. Uočite kako na raspolaganju imate maksimalan broj od 2 termina u tjednu.",
   },
 ];
